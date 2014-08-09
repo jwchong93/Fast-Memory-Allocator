2b1e4175999f9f3105cb64160dd23f2506837e3e
Jing Wen <jwchong93@gmail.com>
2014-08-09 09:40:31 +0800

    Added documentation to AVLInt.c

--- src/AVLInt.c
+++ src/AVLInt.c
@@ -1,6 +1,19 @@
 #include "AVLInt.h"
 #include "manageAVL.h"
 
+/**********************************************************************
+* Compare the integer contain in two node
+*
+*	Input: nodeInTree		the node inside the AVL tree
+*			nodeToCompare	the incoming node that might added to AVL tree
+*	Output: 1			indicated integer contain in nodeIntree is bigger
+*			-1			indicated integer contain in nodeToCompare is bigger
+*			0			integer in both node are the same
+*	
+*	Destroy: none
+*
+***********************************************************************/
+
 int compareInt(void*nodeInTree,void* nodeToCompare)
 {
 	Node* nodeA=(Node*)nodeInTree;
