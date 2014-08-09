d12c33c9a2a55b163cadd800509b9ea7c8e0e37a
Jing Wen <jwchong93@gmail.com>
2014-08-09 09:39:27 +0800

    Added documentation to AVLAddress.c

--- src/AVLAddress.c
+++ src/AVLAddress.c
@@ -1,6 +1,19 @@
 #include "AVLAddress.h"
 #include "Allocator.h"
 
+/**********************************************************************
+* Compare the address contain in two node
+*
+*	Input: nodeInTree		the node inside the AVL tree
+*			nodeToCompare	the incoming node that might added to AVL tree
+*	Output: 1			indicated address contain in nodeIntree is bigger
+*			-1			indicated address contain in nodeToCompare is bigger
+*			0			address in both node are the same
+*	
+*	Destroy: none
+*
+***********************************************************************/
+
 int compareHeader(void *nodeInTree, void *nodeToCompare){
   if(getMemoryAddress(nodeInTree) > getMemoryAddress(nodeToCompare))
     return 1;
@@ -9,6 +22,19 @@ int compareHeader(void *nodeInTree, void *nodeToCompare){
   else 
     return 0;
 }
+
+/**********************************************************************
+* Compare the address contain in two node whether it is suitable to merge
+*
+*	Input: nodeInTree		the node inside the AVL tree
+*			nodeToCompare	the incoming node that going to merge with suitable node.
+*	Output: 1			indicated address contain in nodeIntree is bigger
+*			-1			indicated address contain in nodeToCompare is bigger
+*			0			address contain in nodeInTree is suitable to merge
+*	
+*	Destroy: none
+*
+***********************************************************************/
 
 int compareNode(void *nodeInTree, void *nodeToCompare)
 {
