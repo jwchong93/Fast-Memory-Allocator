511ddaad19995d3916956dd129ed2e6c36f898cf
Jing Wen <jwchong93@gmail.com>
2014-08-09 09:41:21 +0800

    Added documentation to AVLString.c

--- src/AVLString.c
+++ src/AVLString.c
@@ -1,5 +1,18 @@
 #include "AVLString.h"
 #include <String.h>
+
+/**********************************************************************
+* Compare the string contain in two node
+*
+*	Input: nodeInTree		the node inside the AVL tree
+*			nodeToCompare	the incoming node that might added to AVL tree
+*	Output: 1			indicated string contain in nodeIntree is bigger
+*			-1			indicated string contain in nodeToCompare is bigger
+*			0			string in both node are the same
+*	
+*	Destroy: none
+*
+***********************************************************************/
 int compareString(void *nodeInTree, void *nodeToCompare){
   NodeString *node1 = (NodeString *)nodeInTree;
   NodeString *node2 = (NodeString *)nodeToCompare;
